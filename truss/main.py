23761d37a080454b2de83aaf48e720e174068ef8
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-02-12 14:00:03 -0600

    creating empty reports

---
+++ truss/main.py
@@ -0,0 +1,24 @@
+from typing_extensions import Annotated
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def file(
+    file: str,
+    output: Annotated[
+        str, typer.Option(help="The file to output the report to")
+    ] = "report.txt",
+):
+    with open(output, "w+") as f:
+        f.write("Empty report")
+
+
+@app.command()
+def repl():
+    print("repl")
+
+
+def entry():
+    app()
