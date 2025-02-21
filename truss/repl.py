a4ab2e997a389f3636d396428d480c9a821e52ae
Isaac Spencer <22ispencer@gmail.com>
2025-02-21 12:10:18 -0600

    basic completion working

--- truss/repl.py
+++ truss/repl.py
@@ -1,8 +1,34 @@
 from prompt_toolkit import PromptSession, print_formatted_text as print
+from prompt_toolkit.completion import Completer, Completion, CompleteEvent
+from prompt_toolkit.document import Document
+from typing import Iterable
 
-session = PromptSession()
+
+class CommandCompleter(Completer):
+    def get_completions(
+        self, document: Document, complete_event: CompleteEvent
+    ) -> Iterable[Completion]:
+        commands = ["new", "delete"]
+        objects = ["node", "reaction", "force", "member"]
+        input_command = document.current_line.split(" ")
+        if len(input_command) == 1:
+            for command in commands:
+                if command.startswith(input_command[0]):
+                    yield Completion(command, start_position=-len(input_command[0]))
+        elif len(input_command) == 2:
+            for obj in objects:
+                if obj.startswith(input_command[1]):
+                    yield Completion(obj, start_position=-len(input_command[1]))
+
+
+completer = CommandCompleter()
+
+session = PromptSession(completer=completer)
 
 
 def main_loop():
-    in1 = session.prompt("> ")
-    print(in1)
+    command: str = session.prompt(
+        "> ",
+    )
+    for chunk in command.split(" "):
+        print(chunk)
