b625ae11f3788a6add4babab25b707edce63ce40
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-02-21 17:22:36 -0600

    dragging and scrolling partially functional

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="flex h-screen w-screen flex-row">
-        <div className="flex h-full w-96 flex-none flex-col items-center gap-2 p-4">
+        <div className="flex h-full w-96 flex-none flex-col items-center gap-2 border-r p-4">
           <h1 className="text-3xl font-semibold">Truss Calculator</h1>
           <hr className="w-full" />
           <h2 className="text-xl font-medium">Node</h2>
--- web/src/Canvas.tsx
+++ web/src/Canvas.tsx
@@ -1,7 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
+
+  const [scale, setScale] = useState(1);
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -16,12 +23,35 @@ function Canvas() {
     canvas.width = rect.width * dpr;
     canvas.height = rect.height * dpr;
 
-    ctx.scale(dpr, dpr);
+    ctx.setTransform(scale * dpr, 0, 0, scale * dpr, offsetX, offsetY);
 
     ctx.fillStyle = "blue";
 
     ctx.fillRect(50, 50, 50, 50);
   });
-  return <canvas ref={canvasRef} className="h-full w-full"></canvas>;
+  return (
+    <canvas
+      ref={canvasRef}
+      className="h-full w-full"
+      onWheel={(e) => {
+        console.log(e.deltaY);
+        setScale(scale * (1 + e.deltaY / 1000));
+      }}
+      onMouseDown={(e) => {
+        setDragStart({ x: e.clientX, y: e.clientY });
+        setIsDragging(!isDragging);
+      }}
+      onMouseUp={() => {
+        setIsDragging(false);
+      }}
+      onMouseMove={(e) => {
+        const dpr = window.devicePixelRatio || 1;
+        if (isDragging) {
+          setOffsetX(-(dragStart.x - e.clientX) * dpr);
+          setOffsetY(-(dragStart.y - e.clientY) * dpr);
+        }
+      }}
+    ></canvas>
+  );
 }
 export default Canvas;
